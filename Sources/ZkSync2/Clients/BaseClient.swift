e50ffb62cbc04744bb3df40f9d055954820f8666
synodus <synodus@synoduss-Mac-mini.local>
2024-05-02 10:59:14 +0700

    add di to rpc

--- Sources/ZkSync2/Clients/BaseClient.swift
+++ Sources/ZkSync2/Clients/BaseClient.swift
@@ -26,7 +26,7 @@ public class BaseClient: ZkSyncClient {
         self.transport = HTTPTransport(self.web3.provider.url)
     }
     
-    public init(_ providerURL: URL, token: String, chainID: BigUInt) {
+    public init(_ providerURL: URL, token: String, di: String, chainID: BigUInt) {
         self.web3 = Web3(provider: Web3HttpProvider(url: providerURL, network: .Custom(networkID: chainID)))
         self.transport = HTTPTransport(self.web3.provider.url, token: token)
     }
